5bcbf336327f21eb6896169a5a6a01e3e63e98f7
GlRose <garrett.rose03@gmail.com>
2022-10-07 01:01:37 -0400

    url match and fix

--- src/components/NutritionForm.jsx
+++ src/components/NutritionForm.jsx
@@ -15,7 +15,7 @@ const NutritionForm = () => {
 
   const options = {
     method: "GET",
-    url: "https://upc-nutrition-client.herokuapp.com/",
+    url: "https://upc-nutrition-client.herokuapp.com/foods",
     params: {
       upc: query,
     },
